80b8e5c56bbb11b6d2ef252ec9545273c73ec8af
Franklin Harvey <franklinadamsharvey@gmail.com>
2017-04-28 10:12:39 -0600

    no need for csv_to_nc_95_to_17.py to make selections

--- csv_to_nc_95_to_17.py
+++ csv_to_nc_95_to_17.py
@@ -3,38 +3,15 @@ This module takes no arguments. It should be used to convert backlogged data.
 This can be updated by appending new years to the years array.
 '''
 
-import sys
 import os
 import time
 
-def main(selection=""):
-    selectSite = selection[0:3]
-    selectYear = selection[3:]
+def main():
     years = ["95","96","97","98","99","00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17"]
     # the years to be converted
     sites = ["bon","tbl","dra","fpk","gwn","psu","sxf"]
     # the sites to be converted, as found in file names
 
-    if selection:
-        pass
-        # for site in sites[selectSite:]:
-        #     if site == "dra" or site == "psu":
-        #         # these two sites only date back to 1998
-        #         for year in years[3:]:
-        #             call = "python csv_to_nc.py Data/csv/%s%s*.csv" % (site,year)
-        #             os.system(call)
-        #     elif site == "sfk":
-        #         # this site only dates back to 2003
-        #         for year in years[8:]:
-        #             call = "python csv_to_nc.py Data/csv/%s%s*.csv" % (site,year)
-        #             os.system(call)
-        #     else:
-        #         # all other sites date back to 1993
-        #         for year in years:
-        #             call = "python csv_to_nc.py Data/csv/%s%s*.csv" % (site,year)
-        #             os.system(call)
-
-    else:
         for site in sites:
             if site == "dra" or site == "psu":
                 # these two sites only date back to 1998
@@ -53,4 +30,4 @@ def main(selection=""):
                     os.system(call)
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main()
