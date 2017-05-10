912d98add9a55b4a542cfbe23128c5ea60317bad
Franklin Harvey <franklinadamsharvey@gmail.com>
2017-05-10 09:56:23 -0600

    dat_to_csv writes to monthly folders

--- dat_to_csv.py
+++ dat_to_csv.py
@@ -1,6 +1,7 @@
 import sys
 from os.path import basename
 import os
+import datetime
 
 def main(filesToProcess):
 	if len(filesToProcess)>1: # if list of 2 or more files
@@ -10,13 +11,16 @@ def main(filesToProcess):
 		dat_to_csv(filesToProcess[0])
 
 def dat_to_csv(input):
-    headers = get_headers()
-    base = os.path.splitext(basename(input))[0]
+	headers = get_headers()
+	base = get_filename(input)
+	month = get_month(input)
+	year = get_year(input)
+	site = get_site(input)
+	out_name = "Data/csv/%s/%s/%s/%s.csv" % (site, year, month, base)
 
-    print "Processing %s" % base
-    out_name = ("Data/csv/" + base + ".csv")
+	print "Processing %s" % base
 
-    with open(input, 'r') as input_file:
+	with open(input, 'r') as input_file:
 		with open(out_name, 'w') as output_file:
 			output_file.write(headers + "\n")
 			for count, line in enumerate(input_file):
@@ -38,6 +42,43 @@ def get_headers(input="headers.dat", get_csv=True):
         return ('%s') % ','.join(headers)
     else: # leave the option of returning an array of headers instead
         return headers
+
+def get_date_object(input):
+    '''
+    Returns the date object of the file
+    For example, bon95001.csv returns 'datetime.datetime(1995, 12, 31, 0, 0)'
+    '''
+    date = get_filename(input)[3:]
+    return datetime.datetime.strptime(date, '%y%j')
+
+def get_year(input):
+    '''
+    Returns the year of the file
+    For example, bon95001.csv returns '1995'
+    '''
+    date = get_date_object(input)
+    return date.strftime('%Y')
+
+def get_month(input):
+    '''
+    Returns the month of the file
+    For example, bon95001.csv returns 'January'
+    '''
+    date = get_date_object(input)
+    return date.strftime('%B')
+
+def get_site(input):
+	return get_filename(input)[0:3]
+
+def get_filename(input):
+	'''
+	Returns the name of the file without the extension.
+	For example:
+
+	Input: "Test_01.dat"
+	Output: "Test_01"
+	'''
+	return os.path.splitext(basename(input))[0]
 
 if __name__ == '__main__':
     main(sys.argv[1:])
